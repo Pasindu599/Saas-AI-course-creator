a0e76895aad338dc0bd28e9cf8063b2122237e22
Sajith <sajithanuradha860@gmail.com>
2024-08-18 18:35:50 +0530

    add the navbar

---
+++ components/SigninButton.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+
+import React from 'react'
+import { Button } from './ui/button'
+import { signIn } from 'next-auth/react'
+
+type Props = {}
+
+const SigninButton = (props: Props) => {
+  return (
+    <Button variant='ghost' onClick={()=>{
+      signIn("google");
+    }} >
+
+    </Button>
+  )
+}
+
+export default SigninButton
